d27d25de57bfdd766c36968b65fff7bc13f3e1cb
towaldi <tobiaswalddoerfer@gmail.com>
2023-08-11 20:14:58 +0200

    first object implementation

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@
 
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MovableObject();
 
 
 /**
@@ -17,9 +17,6 @@ let character = new Image();
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    character.src = '../img/2_character_pepe/2_walk/W-21.png';
 
-    setTimeout( function() {
-        ctx.drawImage(character, 20, 20, 30, 60);
-    }, 2000);
+    console.log('My character is', character);
 }
---
+++ models/movable-objects.class.js
@@ -0,0 +1,17 @@
+/**
+ * Template
+ * 
+ * Variables: osition x, position y and image
+ * Function
+ */
+
+
+class MovableObject {
+    posX;
+    posY;
+    img;
+
+    moveRight() {
+        console.log('moving right');
+    }
+}
