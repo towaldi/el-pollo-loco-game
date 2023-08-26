b3a05fdba164610ca2089f8c98b9cf0feacd62a6
towaldi <tobiaswalddoerfer@gmail.com>
2023-08-26 15:55:48 +0200

    insert background objects

--- models/world.class.js
+++ models/world.class.js
@@ -16,7 +16,23 @@ class World {
         new BackgroundObject('img/5_background/layers/air.png', 0),
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
+        // Next images (2)
+        new BackgroundObject('img/5_background/layers/air.png', 719),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
+
+        new BackgroundObject('img/5_background/layers/air.png', 719*2),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719*2),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719*2),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719*2),
+        // Next images (2)
+        new BackgroundObject('img/5_background/layers/air.png', 719*3),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719*3),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719*3),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719*3),
+
     ];
     ctx;
     canvas;
