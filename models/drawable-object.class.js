34c7cdbd7895d05164c28f9765f65c60c42f4e5b
towaldi <tobiaswalddoerfer@gmail.com>
2023-09-15 07:37:36 +0200

    status bar - structural changes

---
+++ models/drawable-object.class.js
@@ -0,0 +1,34 @@
+class DrawableObject {
+    posX = 0;
+    posY = 80;
+    height = 60;
+    width = 50;
+    img;
+    imageCache = {};
+    currentImage = 0;
+
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+
+    draw(ctx) {
+        ctx.drawImage(this.img, this.posX, this.posY, this.width, this.height);
+    }
+
+
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+     */
+
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+    }
+}
--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -5,15 +5,8 @@
  * Function
  */
 
-class MovableObject {
+class MovableObject extends DrawableObject {
 
-    posX = 0;
-    posY = 80;
-    height = 60;
-    width = 50;
-    img;
-    imageCache = {};
-    currentImage = 0;
     speed = 0.1;
     otherDirection = false;
     speedPosY = 0;
@@ -34,17 +27,6 @@ class MovableObject {
 
     isAboveGround() {
         return this.posY < 120;
-    }
-
-
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.posX, this.posY, this.width, this.height);
     }
 
 
@@ -88,20 +70,6 @@ class MovableObject {
 
     isDead() {
         return this.energy == 0;
-    }
-
-
-    /**
-     * 
-     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
-     */
-
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
     }
 
 
