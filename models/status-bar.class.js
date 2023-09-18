0f9999b64ea59ddc0b267d3f9830cf2025ce6fb0
towaldi <tobiaswalddoerfer@gmail.com>
2023-09-18 08:53:50 +0200

    status bar III

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -1,3 +1,43 @@
-class StatusBar {
+class StatusBar extends DrawableObject {
+
+    statusbar_images = [
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
+        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
+    ];
+
+
+    percentage = 100;
     
+    
+    constructor() {
+        this.loadImages(this.statusbar_images);
+    }
+
+
+    setPercentage(percentage) {
+        this.percentage = percentage;
+        let path = this.statusbar_images[this.resolveImageIndex()];
+        this.img = this.imageCache[path];
+    }
+
+
+    resolveImageIndex() {
+        if(this.percentage == 100) {
+            return 5;
+        } else if (this.percentage > 80) {
+            return 4;
+        } else if (this.percentage > 60) {
+            return 3;
+        } else if (this.percentage > 40) {
+            return 2;
+        } else if (this.percentage > 20) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
