8d5321a0d8ccc4c2e189dc1ce1b7ab6171ca3773
towaldi <tobiaswalddoerfer@gmail.com>
2023-10-13 12:15:32 +0200

    add controls - html/css

--- models/world.class.js
+++ models/world.class.js
@@ -63,6 +63,10 @@ class World {
         this.ctx.translate(this.cameraPosX, 0);
 
         this.addObjectsToMap(this.level.backgroundObjects);
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.level.enemies);
+        this.addObjectsToMap(this.level.clouds);
+        this.addObjectsToMap(this.throwableObjects);
 
         this.ctx.translate(-this.cameraPosX, 0);
         this.addToMap(this.statusBar);
@@ -70,11 +74,6 @@ class World {
         this.addToMap(this.bottlebar);
         this.addToMap(this.endbossBar);
         this.ctx.translate(this.cameraPosX, 0);
-
-        this.addToMap(this.character);
-        this.addObjectsToMap(this.level.enemies);
-        this.addObjectsToMap(this.level.clouds);
-        this.addObjectsToMap(this.throwableObjects);
 
         this.ctx.translate(-this.cameraPosX, 0);
 
