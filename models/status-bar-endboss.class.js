9fd37dd0fb896f9a7b65896b64020f6f79bde629
Tobias Walddörfer <125135663+towaldi@users.noreply.github.com>
2023-11-03 13:31:19 +0100

    update status bar endboss class

--- models/status-bar-endboss.class.js
+++ models/status-bar-endboss.class.js
@@ -1,3 +1,8 @@
+/**
+ * EndbossBar class represents the status bar of the endboss object
+ * @extends StatusBar
+ */
+
 class EndbossBar extends StatusBar {
 
     statusbar_images = [
@@ -9,8 +14,14 @@ class EndbossBar extends StatusBar {
         'img/7_statusbars/2_statusbar_endboss/blue/100.png'
     ];
 
-
     percentage = 100;
+
+
+    /**
+     * Initializes the status bar of the bottle object
+     * -> Loads images
+     * -> Sets initial percentage to 100
+     */
 
     constructor() {
         super();
@@ -20,5 +31,18 @@ class EndbossBar extends StatusBar {
         this.posY = 27;
         this.width = 170;
         this.height = 34;
+    }
+
+
+    /**
+     * Updates the precentage property of the status bar object
+     * -> Changes displayed image according to health percentage
+     * @param {number} percent -> Health percentage of the endboss
+     */
+
+    setPercentage(percent) {
+        this.percent = percent;
+        let path = this.statusbar_images[this.resolveImageIndexHealth()];
+        this.img = this.imageCache[path];
     }
 }
