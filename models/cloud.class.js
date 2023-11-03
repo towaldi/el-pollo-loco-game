1e30e41a9ff2953c6d20a870deb30e399499391f
Tobias Walddörfer <125135663+towaldi@users.noreply.github.com>
2023-11-03 09:29:40 +0100

    update cloud class

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,19 +1,40 @@
+/**
+ * Cloud class that extends 'MovableObjects'
+ * The class is in the background
+ * @extends MovableObject
+ */
+
 class Cloud extends MovableObject {
 
     posY = 20;
     width = 480;
     height = 240;
 
+
+    /**
+     * Initializes the background cloud object
+     * -> Loads the images + sets it's initial position + speed
+     * -> Sets up 'animate()'
+     */
+
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
-        
-        this.posX = Math.random() * 360;
+        this.posY = 10 + Math.random() * 50;
+        this.posX = Math.random() * 2000;
+        // this.speed = 0.05;
         this.animate();
     }
 
 
+    /**
+     * Responsible for the animation of the cloud
+     * -> Uses 'setInterval()' to move cloud to the left at 60 frames per second
+     */
+
     animate() {
-        this.moveLeft();
+        setInterval(() => {
+            this.moveLeft();
+            this.otherDirection = false;
+        }, 1000 / 60);
     }
-    
 }
