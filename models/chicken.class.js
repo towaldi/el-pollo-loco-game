9997a58723a2d10f2c91ff40b26c65a4962894bf
Tobias Walddörfer <125135663+towaldi@users.noreply.github.com>
2023-11-03 09:11:52 +0100

    update chicken class

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,3 +1,8 @@
+/**
+ * Chicken class that extends 'MovableObjects'
+ * @extends MovableObject
+ */
+
 class Chicken extends MovableObject {
     
     posY = 350;
@@ -10,9 +15,21 @@ class Chicken extends MovableObject {
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
 
+    images_dead = [
+        'img/3_enemies_chicken/chicken_normal/2_dead/dead.png',
+    ];
+
+    /**
+     * Initializes the chicken object by loading the walking + dead images
+     * -> Sets initial x position randomly between 360 and 2360
+     * -> Sets speed randomly between 0.1 and 0.5
+     * -> Sets up 'chickenAnimation()'
+     */
+
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.images_walking);
+        this.loadImages(this.images_dead);
         
         this.posX = 360 + Math.random() * 2000;
         this.speed = 0.1 + Math.random() * 0.4;
